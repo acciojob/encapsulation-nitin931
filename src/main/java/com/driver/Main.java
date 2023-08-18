f87a52cb271d839cbfb7742fec389e35822f3507
nitin931 <nitinthakur931@gmail.com>
2023-08-18 16:31:55 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args){
+        RWOnly rw = new RWOnly();
+        rw.setName("Nitin");
+        rw.getName();
+    }
   
 }
